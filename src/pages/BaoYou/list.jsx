6f835adc5fc4d710f3d0f5ee3273b1c77d7a3da1
Kelly-yjx <122753656@qq.com>
2019-07-27 09:14:50 +0800

    zaoshang

--- src/pages/BaoYou/list.jsx
+++ src/pages/BaoYou/list.jsx
@@ -9,8 +9,8 @@ class List extends Component {
         }
     }
     async componentWillMount() {
-        let { data } = await Axios.get('http://localhost:1904/index.php?r=nine/listajax&n_id=58&page=2&cac_id=cXVlcnlUaGVuRmV0Y2g7Njs5OTI0MTE4MDA6NldLYVpVNHVRVEstYmxiWkdueU9HZzs4MDYxOTY2ODQ6NFIxVmgxc2FTelNmNXN1SjlCVXFrZzsxMTk2Nzc2ODMwOjFJSGNjLV9lUTF5bzNTOFRMaEY2QVE7ODA2MTk2NjgzOjRSMVZoMXNhU3pTZjVzdUo5QlVxa2c7OTkyNDExODAyOjZXS2FaVTR1UVRLLWJsYlpHbnlPR2c7MTU2MDM3NDY0Nzp1aUxxcHNfRFJXNlVWb0JhbHlVOUF3OzA7')
-        // console.log(data.data.data)
+        let { data } = await Axios.get('http://localhost:1904/index.php?r=nine/listajax&n_id=58&page=3&cac_id=cXVlcnlUaGVuRmV0Y2g7NjsxMDI4MzQ1MTE0OjZXS2FaVTR1UVRLLWJsYlpHbnlPR2c7ODQyMTMxMzEyOjRSMVZoMXNhU3pTZjVzdUo5QlVxa2c7MTIxNDgwNDkzOToxSUhjYy1fZVExeW8zUzhUTGhGNkFROzg0MjEzMTMxMzo0UjFWaDFzYVN6U2Y1c3VKOUJVcWtnOzEwMjgzNDUxMDg6NldLYVpVNHVRVEstYmxiWkdueU9HZzsxNTc4NDAyMDQ4OnVpTHFwc19EUlc2VVZvQmFseVU5QXc7MDs%3D')
+        // console.log(data.data.data)  
         let GoodList = data.data.data.map(item => item);
         console.log(GoodList)
         this.setState({
@@ -19,6 +19,7 @@ class List extends Component {
     }
     render() {
         let {GoodList} = this.state
+        console.log(GoodList)
         return <div className="goodlist">
             <h3>精选推荐</h3>
             <ul className="list">
@@ -28,7 +29,7 @@ class List extends Component {
                         <p>{item.title}</p>
                     </li>
                 ))}
-
+               
 
             </ul>
         </div>
