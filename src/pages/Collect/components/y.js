45fd3f43981238d34d97e4d85ea1b14119bcf785
Megustas <2816850830@qq.comgit config --global user.email 2816850830@qq.com>
2019-08-02 18:49:00 +0800

    收藏列表全选

--- src/pages/Collect/components/y.js
+++ src/pages/Collect/components/y.js
@@ -2,18 +2,27 @@ import React,{Component} from 'react'
 
 import {withRouter} from 'react-router-dom'
 
+import { Checkbox } from 'antd';
+
+
 // import axios from 'axios'
 
 import './y.css'
 // import './w.css'
 
+const plainOptions = ['Apple', 'Pear', 'Orange'];
+const CheckboxGroup = Checkbox.Group;
 class  Y extends Component{
   constructor(){
     super();
     this.state = {
-      isShow:"block"
+      isShow:"block",
+      inters:[],
+      intersAll:[],
+      checkAll: false,
     }
     this.toHome = this.toHome.bind(this)
+   
   }
 async componentWillMount(){
   // let {data} = await axios.get("http://m.hlxns.com/m/index.php?r=user/listajax")
@@ -25,6 +34,18 @@ async componentWillMount(){
       pathname:'/home'
     })
   }
+  onChange = (e) => {
+    let checked = e.target.checked;
+    let {inters} = this.state
+    if(checked && !inters.includes(e.value)){
+      inters.push(e.value);
+    }else{
+      inters =  inters.filter(item=>e.value != item)
+    }
+    this.setState({
+      inters
+    });
+  };
     render(){
       return (
         <div className="layout-shoucang">
@@ -41,7 +62,9 @@ async componentWillMount(){
        </li>
        <li className="row-s">
          <div className="label">
-           <label></label>
+         <label>
+           <input type="checkbox" className="checkbox-input" onChange={this.onChange} value={a} checked={this.state.inters.includes(a)}/>
+         </label>
            <p className="sc-img">
              <a>
                <img src="https://img.alicdn.com/imgextra/i4/3054826079/O1CN01bEKAgy1umEDcoAXKg_!!0-item_pic.jpg_310x310.jpg_.webp"/>
